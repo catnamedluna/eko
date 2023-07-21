6ad7a3a5cc2d22001d4c04c3ba50d875e6a56377
luna <108822968+catnamedluna@users.noreply.github.com>
2023-07-21 02:02:22 -0500

    12

--- apiHandler.js
+++ apiHandler.js
@@ -6,4 +6,27 @@ router.get('/', (request, response) => {
 	return response.send('hi')
 });
 
+router.get('/discord/user/:code', (request, response) => {
+    fetch(process.env.DISCORD_API_ENDPOINT, {
+        method: "POST",
+        cache: "default",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        redirect: "manual",
+        body: {
+            "client_id": process.env.APP_ID,
+            "client_secret": process.env.APP_SECRET,
+            "grant_type": "authorization_code",
+            "code": authCode,
+            "redirect_uri": process.env.REDIRECT_URI
+        }
+    }).then((res) => {
+        return response.send(JSON.stringify(res))
+    })
+
+	return response.send('hi')
+});
+
 module.exports = router
